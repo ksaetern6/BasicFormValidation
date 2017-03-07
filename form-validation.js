af8335dcdc92da6a3c029b1314e4e405529ad507
Unknown <ksaetern6@mail.csuchico.edu>
2017-03-07 11:41:37 -0800

    Changed .js file
    Add two if-else statements to meet the requirements of user input.

--- form-validation.js
+++ form-validation.js
@@ -4,16 +4,25 @@ function validate(){
   var passEntered = document.getElementById("pass").value;
 
   //Show message that there is an error with the username...
-  document.getElementById("usernameError").innerHTML="Bad username.";
-  document.getElementById("usernameError").classList.remove("hidden-message");
-  document.getElementById("usernameError").classList.add("shown-message");
-  //Turn the username items red
-  document.getElementById("usernameGroup").classList.add("has-error");
-
+  if(userEntered.length<6){
+    document.getElementById("usernameError").innerHTML="Bad username.";
+    document.getElementById("usernameError").classList.remove("hidden-message");
+    document.getElementById("usernameError").classList.add("shown-message");
+    //Turn the username items red
+    document.getElementById("usernameGroup").classList.add("has-error");
+  }
+  else{
+    document.getElementById("usernameGroup").classList.add("has-success");
+  }
+  if(passEntered=="password"){
   //Show message that there is an error with the password...
-  document.getElementById("passwordError").innerHTML="Bad password.";
-  document.getElementById("passwordError").classList.remove("hidden-message");
-  document.getElementById("passwordError").classList.add("shown-message");
-  //Turn the password items red
-  document.getElementById("passwordGroup").classList.add("has-error");
+    document.getElementById("passwordError").innerHTML="Bad password.";
+    document.getElementById("passwordError").classList.remove("hidden-message");
+    document.getElementById("passwordError").classList.add("shown-message");
+    //Turn the password items red
+    document.getElementById("passwordGroup").classList.add("has-error");
+  }
+  else{
+    document.getElementById("passwordGroup").classList.add("has-success");
+  }
 }
